85453344179b3ed4b9c6b6a9a0327acad1d27889
Thanoj Singh <thanoj.singh@gmail.com>
2023-04-13 00:03:52 -0400

    commit

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	go func() {
 		w := app.NewWindow(
 			app.Title("Canvas"),
-			app.Size(unit.Dp(650), unit.Dp(600)),
+			app.Size(unit.Dp(600), unit.Dp(600)),
 		)
 
 		if err := draw(w); err != nil {
@@ -35,49 +35,9 @@ func main() {
 
 func draw(w *app.Window) error {
 	var ops op.Ops
-	bv1 := &ButtonVisual{
-		roundness: 2,
-		pressed:   false,
-		tag:       new(bool),
-		width:     0,
-		height:    0,
-	}
 
-	bv2 := &ButtonVisual{
-		roundness: 0,
-		pressed:   false,
-		tag:       new(bool),
-		width:     300,
-		height:    510,
-	}
-
-	bv3 := &ButtonVisual{
-		roundness: 0,
-		pressed:   false,
-		tag:       new(bool),
-		width:     500,
-		height:    510,
-	}
-
-	/*
-		bv4 := &ButtonVisual{
-			roundness: 0,
-			pressed:   false,
-			tag:       new(bool),
-			width:     500,
-			height:    510,
-		}*/
-
+	c := widget.Clickable{}
 	th := material.NewTheme(gofont.Collection())
-
-	window := Window{
-		tag: new(bool),
-		list: widget.List{Scrollbar: widget.Scrollbar{}, List: layout.List{
-			Axis: layout.Vertical,
-		}},
-		theme:   th,
-		widgets: []layout.Widget{bv2.Layout, bv3.Layout},
-	}
 
 	for windowEvent := range w.Events() {
 		switch e := windowEvent.(type) {
@@ -86,16 +46,9 @@ func draw(w *app.Window) error {
 			gtx := layout.NewContext(&ops, e)
 
 			op.Offset(image.Point{X: 10, Y: 10}).Add(&ops)
-
 			paint.Fill(&ops, color.NRGBA{R: 0xff, G: 0xfe, B: 0xe0, A: 0xff})
 
-			bv1.Layout(gtx)
-			op.Offset(image.Point{X: 0, Y: 200}).Add(&ops)
-			window.Layout(gtx)
-
-			op.Offset(image.Point{X: 0, Y: 200}).Add(&ops)
-
-			//bv3.Layout(gtx)
+			material.Button(th, &c, "Click me").Layout(gtx)
 
 			e.Frame(gtx.Ops)
 		case system.DestroyEvent:
