a4a9dcd723f592e92178c435839049a3c9500898
Thanoj Singh <thanoj.singh@gmail.com>
2023-04-13 23:34:44 -0400

    commit

--- main.go
+++ main.go
@@ -45,9 +45,22 @@ func draw(w *app.Window) error {
 		case system.FrameEvent:
 			gtx := layout.NewContext(&ops, e)
 
+			gtx.Constraints.Min = image.Point{
+				X: 100,
+				Y: 100,
+			}
+
+			gtx.Constraints.Max = image.Point{
+				X: 100,
+				Y: 100,
+			}
+
 			op.Offset(image.Point{X: 10, Y: 10}).Add(&ops)
 			paint.Fill(&ops, color.NRGBA{R: 0xff, G: 0xfe, B: 0xe0, A: 0xff})
 
+			material.Button(th, &c, "Click me, click me, click me, click me, click me").Layout(gtx)
+
+			op.Offset(image.Point{X: 100, Y: 100}).Add(&ops)
 			theme.Button(th, &c, "Click me").Layout(gtx)
 
 			e.Frame(gtx.Ops)
