c6083b68fbbba3e17c69e83f8865aebbc4a3c607
Thanoj Singh <thanoj.singh@gmail.com>
2023-04-14 15:47:09 -0400

    commit

--- main.go
+++ main.go
@@ -58,10 +58,9 @@ func draw(w *app.Window) error {
 			op.Offset(image.Point{X: 10, Y: 10}).Add(&ops)
 			paint.Fill(&ops, color.NRGBA{R: 0xff, G: 0xfe, B: 0xe0, A: 0xff})
 
-			material.Button(th, &c, "Click me, click me, click me, click me, click me").Layout(gtx)
-
+			theme.Button(th, &c, "Click me, click me").Layout(gtx)
 			op.Offset(image.Point{X: 100, Y: 100}).Add(&ops)
-			theme.Button(th, &c, "Click me").Layout(gtx)
+			material.Button(th, &c, "Click me, click me, click me, click me, click me").Layout(gtx)
 
 			e.Frame(gtx.Ops)
 		case system.DestroyEvent:
