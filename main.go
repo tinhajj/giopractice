983f541bc603f5c8a0a6ccb5f00433c88f13ae2a
Thanoj Singh <thanoj.singh@gmail.com>
2023-04-16 01:02:38 -0400

    commit

--- main.go
+++ main.go
@@ -59,8 +59,11 @@ func draw(w *app.Window) error {
 
 			paint.Fill(&ops, color.NRGBA{R: 0xff, G: 0xfe, B: 0xe0, A: 0xff})
 
+			theme.Button(th, &c, "Click me, click me").Layout(gtx)
+			op.Offset(image.Point{X: 0, Y: 200}).Add(&ops)
+
 			layout.Stack{}.Layout(gtx,
-				layout.Expanded(func(gtx layout.Context) layout.Dimensions {
+				layout.Stacked(func(gtx layout.Context) layout.Dimensions {
 					return theme.Button(th, &c, "Click me, click me").Layout(gtx)
 				}),
 			)
