b5e305c69842308a1cc71f065ef3918b471bd112
Thanoj Singh <thanoj.singh@gmail.com>
2023-04-16 01:12:46 -0400

    colors

---
+++ pallete.go
@@ -0,0 +1,12 @@
+package main
+
+import "image/color"
+
+type Palette struct {
+	Light      color.NRGBA
+	Neutral    color.NRGBA
+	Dark       color.NRGBA
+	Accent     color.NRGBA
+	Foreground color.NRGBA
+	Background color.NRGBA
+}
