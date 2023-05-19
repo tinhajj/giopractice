fdbdfaaea81d0d702e4a6669f9d6af3e8ebf231a
Thanoj Singh <thanoj.singh@gmail.com>
2023-05-19 00:12:39 -0400

    commit

--- main.go
+++ main.go
@@ -8,12 +8,10 @@ import (
 	"ui/widget"
 
 	"gioui.org/app"
-	"gioui.org/font/gofont"
 	"gioui.org/io/system"
 	"gioui.org/layout"
 	"gioui.org/op"
 	"gioui.org/op/paint"
-	"gioui.org/text"
 	"gioui.org/unit"
 )
 
@@ -40,16 +38,10 @@ func draw(w *app.Window) error {
 	var ops op.Ops
 	//th := material.NewTheme(gofont.Collection())
 
-	fonts := gofont.Collection()
-	shaper := text.NewShaper(fonts)
-
-	font := text.Font{}
-
 	//active := true
 	//b := widget.NewBool(&active)
 
 	win := widget.NewWindow("Hello")
-	label := widget.Label{Alignment: text.Middle}
 
 	for windowEvent := range w.Events() {
 		switch e := windowEvent.(type) {
@@ -59,10 +51,7 @@ func draw(w *app.Window) error {
 
 			paint.Fill(gtx.Ops, color.NRGBA{R: 105, G: 105, B: 105, A: 255})
 
-			textColorMacro := op.Record(gtx.Ops)
-			paint.ColorOp{Color: color.NRGBA{R: 0, B: 0, G: 0, A: 255}}.Add(gtx.Ops)
-			textColor := textColorMacro.Stop()
-			label.Layout(gtx, shaper, font, unit.Sp(30), "Hello", textColor)
+			theme.Label(gtx, unit.Sp(30), "Hello")
 
 			theme.Window(win).Layout(gtx)
 
